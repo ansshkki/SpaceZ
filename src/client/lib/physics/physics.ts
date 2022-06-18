8349dd834c2dd84e4ea784a92b89211f76270926
Obaa10 <70761777+Obaa10@users.noreply.github.com>
2022-06-18 18:16:54 +0000

    Fix

--- src/client/lib/physics/physics.ts
+++ src/client/lib/physics/physics.ts
@@ -10,29 +10,26 @@ export class Physics {
 
     update(bodies: Array<Body>) {
 
-        this.planets(bodies)
 
-
-    }
-
-
-    planets(bodies: Array<Body>){
-        const planets = bodies.map(value => {
-            const pos = new Point(value.pos.x,value.pos.y)
-            return new Planet(value.id.toString(),value.mass * Math.pow(10  ,5),
-                pos, value.vector,value.radius)
-        })
-        Planet_utils.updateAllPlanet(planets,1)
-        bodies.forEach(body => {
-            planets.forEach(planet => {
-                if(body.id.toString() == planet.name){
-                    body.pos.x = planet.position.x
-                    body.pos.y = planet.position.y
-                    body.vector = planet.vector
-                }
-                body.update()
+            const planets = bodies.map(value => {
+                const pos = new Point(value.pos.x,value.pos.y)
+                return new Planet(value.id.toString(),value.mass * Math.pow(10  ,5),
+                    pos, value.vector,value.radius)
             })
-        })
+            Planet_utils.updateAllPlanet(planets,1)
+            bodies.forEach(body => {
+                planets.forEach(planet => {
+                    if(body.id.toString() == planet.name){
+                        body.pos.x = planet.position.x
+                        body.pos.y = planet.position.y
+                        body.vector = planet.vector
+                    }
+                    body.update()
+                })
+            })
+
+
+
     }
 
 
